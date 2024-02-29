5842b827b6fb29b073d29fada6d3d213416ddb87
Ikey Doherty <ikey@thunderbird.net>
2024-02-29 17:05:09 +0000

    NFC: Add license headers to all `.rs` files
    Signed-off-by: Ikey Doherty <ikey@thunderbird.net>

--- exchange/ews/src/xml.rs
+++ exchange/ews/src/xml.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use xml::writer;
 
 use crate::types::{EwsWrite, SOAP_NS_URI, TYPES_NS_URI};
